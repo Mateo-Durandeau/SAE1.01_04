1feb318fb12c245a77686c946286e5a4c0e2afce
mateo <110668071+hitoricp@users.noreply.github.com>
2024-01-18 17:17:30 +0100

    Update script_automatisation_http_to_bdd.py

--- script_automatisation_http_to_bdd.py
+++ script_automatisation_http_to_bdd.py
@@ -7,12 +7,6 @@ import script_requete_download_http
 #
 #           # READ ME
 #
-#       Ce script automatise le téléchargement, la gestion de doublon et l'ajout dans les bases de données avec la supprésion du fichier dans les dossiers
-#
-#       Case 1 = 1 jours 
-#       Case 2 = 7 jours 
-#       Case 3 = 1 mois
-#
 ######################################################################
 
 
@@ -27,11 +21,23 @@ def choix(case, year, month, day):
         script_requete_download_http.download_week(year, month, day)
         script_donnes_csv_data.all_table_add(3, year, month, day)
     
-    script_donnes_csv_data.affiche_mesure_bdd()
+def choix_v2(nombre_jour, year, month, day):
+    day = day
+    day_test = day
+    for i in range(0, nombre_jour):
+        script_requete_download_http.download_day(year, month, day_test)
+        script_donnes_csv_data.all_table_add(1, year, month, day_test)
+        day_test += 1
 
 if __name__=='__main__':
-    case = 2
-    year = 2021
-    month = 4
+
+    nombre_jour = 1
+
+    case = 1
+
+    year = 2024
+    month = 1
     day = 1
+
     choix(case, year, month, day)
+    #choix_v2(nombre_jour, year, month, day)
